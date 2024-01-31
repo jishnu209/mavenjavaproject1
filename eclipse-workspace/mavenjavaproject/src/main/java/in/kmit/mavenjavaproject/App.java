31f143aca0ac1cd389bc465cad46d5feac111366
jishnu <tejaswinikrishnan209@gmail.com>
2024-01-31 21:32:41 +0530

    first commit

---
+++ eclipse-workspace/mavenjavaproject/src/main/java/in/kmit/mavenjavaproject/App.java
@@ -0,0 +1,14 @@
+package in.kmit.mavenjavaproject;
+import java.util.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("I am tejaswini");
+        System.out.println( "Hello World!" );
+    }
+}
